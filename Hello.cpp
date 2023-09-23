54504961ec7690ac90c121864ea040087b3309db
Orb <HeavyObjectics@gmail.com>
2023-09-23 13:11:58 +0300

    function Perimeter complete

--- Hello.cpp
+++ Hello.cpp
@@ -3,26 +3,18 @@
 #include "sotrtAlgorithm.h"
 #include "figure.h"
 #include <cmath>
-int main()
-{
-    /*
-    class algorithm algo;
-    int mas[10]={4,98,2,4,0,124,5,8,32,140};
-    cout<<sizeof(mas)/sizeof(mas[0])<<'\n';
-    int size =sizeof(mas)/sizeof(mas[0]);
-
-    algo.print_mas(mas,sizeof(mas)/sizeof(mas[0]));
-    int algorithm=0;
-    cout<<"write algorithm fo start sort\n";
-    cout<<"1 - insertion sort'\n"<<"2 - quick sort\n"<<"3 - merge sort\n";
-    cin>>algorithm;
-    if(algorithm==1){
-        algo.insertion_sort(mas,sizeof(mas)/sizeof(mas[0]));
-    }else if (algorithm==2)
-    {
-        algo.quicksort(mas,0,size-1);*/
-    //}
-    //algo.print_mas(mas,sizeof(mas)/sizeof(mas[0]));
-    
+int main() {
+    class Shape shape;
+    long x, y,x1,y1,x2,y2;
+    cin >> x >> y;
+    cin>>x1>>y1;
+    cin>>x2>>y2;
+    shape.push_back(x, y);
+    shape.push_back(x1,y1);
+    shape.push_back(x2,y2);
+    cout<<'\n';
+    cout<<shape.perimeter();
+    cout<<'\n';
+    shape.print();
     return 0;
-    }
+}
--- figure.h
+++ figure.h
@@ -2,28 +2,65 @@
 
 class Shape
 {
+protected:
+   static long square(long x)
+    {
+        return (x * x);
+    }
 private:
-std::vector<std::pair<long, long>> points{};
+std::vector<std::pair<long, long> > points;
 public:
 Shape()
 {}
-void push_back(long x,long y){
-    points.push_back(std::pair<long, long>{x, y});
+
+void print()
+{
+    for(int obj=0;obj<points.size();obj++)
+        cout<<'('<<points[obj].first<<';'<<points[obj].second<<")\n";
 }
 
-void push_back(std::pair<long, long> point){
+void push_back(long x,long y)
+{
+    points.push_back(std::pair<long, long>(x, y));
+}
+
+void push_back(std::pair<long, long> point)
+{
     points.push_back(point);
 }
-    friend void perimeter(std::vector<std::pair<long, long>> points){};
-~Shape(){points.clear();};
-};
-long distanceBetweenPoints(std::pair<long, long> point1,std::pair<long, long> point2){
-    long distance =sqrt(square((point2.first-point1.first))+(square(point2.second-point1.second)));
-};
-void perimeter(std::vector<std::pair<long, long>> points){
 
-};
-long square(long x)
+long distanceBetweenPoints(std::pair<long, long> point1,std::pair<long, long> point2)
 {
-return (x * x);
+        return sqrt(std::pow(point2.first-point1.first,2)+std::pow(point2.second-point1.second,2));
+}
+
+long perimeter(std::vector<std::pair<long, long> > points)
+{
+    if(points.size()<3)
+    {
+        cout<<"Неможливо знайти периметер з двох точок\n";
+        return 0;
+    }
+    int i=1;
+    long perimeter=0;
+
+    auto prev = points.begin();
+    auto curr=prev+1;
+
+    while (curr!=points.end())
+    {
+        perimeter+=distanceBetweenPoints(points[i-1],points[i]);
+        perimeter+= distanceBetweenPoints(*prev,*curr);
+        ++prev;
+        ++curr;
+    }
+    perimeter+= distanceBetweenPoints(points.back(),points.front());
+    return perimeter;
+}
+long perimeter(){
+        long perimeter = distanceBetweenPoints(points[0],points[1]);
+        return perimeter;
+    }
+
+~Shape(){points.clear();}
 };
